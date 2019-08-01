cb608aaa325b1cd016facc9d8a0b8ede326d09d5
David Ison <developer@dtison.net>
2019-08-01 19:11:46 -0400

    Implements setInterval()

--- process-monitor.js
+++ process-monitor.js
@@ -10,27 +10,73 @@
   childProcess.spawn = mySpawn;
 })(); */
 
+const 
+Reset = "\x1b[0m",
+Bright = "\x1b[1m",
+Dim = "\x1b[2m",
+Underscore = "\x1b[4m",
+Blink = "\x1b[5m",
+Reverse = "\x1b[7m",
+Hidden = "\x1b[8m",
+
+FgBlack = "\x1b[30m",
+FgRed = "\x1b[31m",
+FgGreen = "\x1b[32m",
+FgYellow = "\x1b[33m",
+FgBlue = "\x1b[34m",
+FgMagenta = "\x1b[35m",
+FgCyan = "\x1b[36m",
+FgWhite = "\x1b[37m",
+
+BgBlack = "\x1b[40m",
+BgRed = "\x1b[41m",
+BgGreen = "\x1b[42m",
+BgYellow = "\x1b[43m",
+BgBlue = "\x1b[44m",
+BgMagenta = "\x1b[45m",
+BgCyan = "\x1b[46m",
+BgWhite = "\x1b[47m";
+
+
 function run(cmd) {
   return new Promise((resolve, reject) => {
       var spawn = require('child_process').spawn;
       var command = spawn(cmd)
+      console.info('New child process PID', command.pid)
       var result = ''
       command.stdout.on('data', function(data) {
+        console.info('\x1b[34m Got stdout', data.toString(), Reset);
            result += data.toString()
       })
       command.on('close', function(code) {
+          console.info('Child process has stopped returned', code);
           resolve(result)
       })
       command.on('error', function(err) { reject(err) })
   })
 }
 
-//run('/bin/ls').then(result=>{console.info(result);}).catch(error=>{console.info (error)});
-run('./worker.php').then(result=>{console.info(result);}).catch(error=>{console.info (error)});
+ 
+  let isActive = false;
+  setInterval(_=> {
 
+    if (isActive) {
+      console.info(FgYellow, 'Worker is still active, skipping run', Reset);
+      return;
+    } 
+    isActive = true;
+    run('./worker.php')
+    .then(result=>{
+      isActive = false;
+      console.info('Worker spawn was successful.');
+
+  })
+  .catch(error=>{console.info (error)});
+  }, 3000);
+ 
 
 // subber.js
-var zmq = require('zeromq')
+/* var zmq = require('zeromq')
   , sock = zmq.socket('sub');
 
 sock.connect('tcp://127.0.0.1:3005');
@@ -41,4 +87,4 @@ console.log('Subscriber connected to port 3005');
 sock.on('message', function(topic, message) {
 
   console.log('received a message type', topic.toString(), 'containing message:', message.toString());
-});
+}); */
--- worker.php
+++ worker.php
@@ -2,27 +2,33 @@
 <?php
 
 
-$context = new ZMQContext();
+/* $context = new ZMQContext();
 $publisher = new ZMQSocket($context, ZMQ::SOCKET_PUB);
 $publisher->bind("tcp://127.0.0.1:3005");
+ */
 
+$max =  rand(1, 50);
 
 $count = 0;
-while (++$count <= 100) {
-  echo "Hello..\n";
-    //  Write two messages, each with an envelope and content
-    $publisher->send("A", ZMQ::MODE_SNDMORE);
+while (++$count <= $max) {
+  echo "Heartbeat  it's $count of $max \n";
+
+/*     $publisher->send("A", ZMQ::MODE_SNDMORE);
     $publisher->send("We don't want to see this");
     $publisher->send("heartbeat", ZMQ::MODE_SNDMORE);
-    $publisher->send("This is a heartbeat message. Status OK."); 
+    $publisher->send("This is a heartbeat message. Status OK. $count");  */
  
 
     sleep (1);
 
 }  
 
+if (($max %5) == 0) {
+  die (2);
+}
+/* 
 $count = 0;
 while (++$count <= 4) {
   echo "$count\n";
   sleep(1);
-}
+} */
